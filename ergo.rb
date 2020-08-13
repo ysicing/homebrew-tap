2c4be7a676300fe909fd7acf3bca9ea5fb40c852
root <root@hk2.vps.godu.dev>
2020-08-13 13:16:18 +0800

    release ergo 0.10.4

--- ergo.rb
+++ ergo.rb
@@ -1,19 +1,19 @@
 class Ergo < Formula
     desc "Devops tools 运维工具"
     homepage "https://github.com/ysicing/ergo"
-    version "0.10.3"
+    version "0.10.4"
     bottle :unneeded
-  
+
     if OS.mac?
       url "https://github.com/ysicing/ergo/releases/download/#{version}/ergo_darwin_amd64"
-      sha256 "784eb8c3b4176493d784bdc22e225c7fa4eae8494deb260b2ba67e897d9285ef"
+      sha256 "cb28d346f98156b7ff03d67a6e8830b8ba7ea1934e9d5a335cd379ef8e91a1c8"
     elsif OS.linux?
       if Hardware::CPU.intel?
         url "https://github.com/ysicing/ergo/releases/download/#{version}/ergo_linux_amd64"
-        sha256 "da657d3203968413b8ef1ffe22fceb3a1cee43138b9d8053366127e3662cd714"
+        sha256 "71b89f43b432b66e20fbc968e401cbef05ac9b47c1518026fc0ef53f4235c413"
       end
     end
-  
+
     def install
       bin.install "ergo_darwin_amd64" => "ergo"
     end
--- genergo.sh
+++ genergo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker pull ysicing/taprb:ergo
-docker run -it -v $PWD:/data --rm ysicing/taprb:ergo cp -a/ergo.rb /data/ergo.rb
+docker run -it -v $PWD:/data --rm ysicing/taprb:ergo cp -a /ergo.rb /data/ergo.rb
 git add .
 version=$(cat ergo.rb | grep "version " | awk -F\" '{print $2}')
 git commit -m "release ergo ${version}"
