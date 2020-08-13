5b28a166a9c826fd3848c6f2273b2577683e1f90
root <root@hk2.vps.godu.dev>
2020-08-13 13:14:19 +0800

    release ergo 0.10.3

---
+++ genergo.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker pull ysicing/taprb:ergo
+docker run -it -v $PWD:/data --rm ysicing/taprb:ergo cp -a/ergo.rb /data/ergo.rb
+git add .
+version=$(cat ergo.rb | grep "version " | awk -F\" '{print $2}')
+git commit -m "release ergo ${version}"
+git push origin master
