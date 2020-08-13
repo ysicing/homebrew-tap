779fbe77770d813b801b29ddb31ee0e1c3f2ac1e
ysicing <i@ysicing.me>
2020-08-13 11:19:49 +0800

    improvement: update code

--- ergo.rb
+++ ergo.rb
@@ -5,16 +5,16 @@ class Ergo < Formula
     bottle :unneeded
   
     if OS.mac?
-      url "https://github.com/ysicing/ergo/releases/download/0.10.3/ergo_darwin_amd64"
+      url "https://github.com/ysicing/ergo/releases/download/#{version}/ergo_darwin_amd64.tar.gz"
       sha256 "784eb8c3b4176493d784bdc22e225c7fa4eae8494deb260b2ba67e897d9285ef"
     elsif OS.linux?
       if Hardware::CPU.intel?
-        url "https://github.com/ysicing/ergo/releases/download/0.10.3/ergo_linux_amd64"
+        url "https://github.com/ysicing/ergo/releases/download/#{version}/ergo_linux_amd64.tar.gz"
         sha256 "da657d3203968413b8ef1ffe22fceb3a1cee43138b9d8053366127e3662cd714"
       end
     end
   
     def install
-      bin.install "ergo"
+      bin.install "ergo_darwin_amd64" => "ergo"
     end
   end
