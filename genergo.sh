1e65aeb87537fd6788dcbb4a1a5a41ddda852e9a
hkbot <hkbot@hk2.vps>
2020-08-13 13:21:09 +0800

    release ergo 0.10.4 by bot 2020-08-13-13-21

--- genergo.sh
+++ genergo.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+set -e
+
+cd /root/homebrew-tap
 docker pull ysicing/taprb:ergo
 docker run -it -v $PWD:/data --rm ysicing/taprb:ergo cp -a /ergo.rb /data/ergo.rb
 git add .
 version=$(cat ergo.rb | grep "version " | awk -F\" '{print $2}')
-git commit -m "release ergo ${version}"
+datetime=$(date +%F-%H-%M)
+git commit -m "release ergo ${version} by bot ${datetime}"
 git push origin master
