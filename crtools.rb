c0645fac58bbdd84a3b90d244b81196c9a4a2408
hkbot <hkbot@hk2.vps>
2020-08-16 08:45:25 +0800

    release crtools 0.0.7 by bot 2020-08-16-08-45

--- crtools.rb
+++ crtools.rb
@@ -1,16 +1,16 @@
 class Crtools < Formula
     desc "Devops tools 运维工具"
     homepage "https://github.com/ysicing/crtools"
-    version "0.0.6"
+    version "0.0.7"
     bottle :unneeded
 
     if OS.mac?
       url "https://github.com/ysicing/crtools/releases/download/#{version}/crtools_darwin_amd64"
-      sha256 "dd7462c8ea38a13c8c05059d95cd4dbddf396a12a5d5fb2aebc9195a2fa82835"
+      sha256 "5c1fd2e4f436e912616cba8f6999504e9a7b65de91b6f419961c00fde1625bbe"
     elsif OS.linux?
       if Hardware::CPU.intel?
         url "https://github.com/ysicing/crtools/releases/download/#{version}/crtools_linux_amd64"
-        sha256 "a1e3552c569152bb2d91c4b9da5886a43de7536f067f02a9e444fa121b77711c"
+        sha256 "641ca06b4a5d231d8c45e6353499a7c6cb67fb530505b524fe2bb7fa302e1501"
       end
     end
 
