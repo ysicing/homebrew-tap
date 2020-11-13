2ee33557bf1dfc48ab25a9421c461cd38e606602
hkbot <hkbot@hk2.vps>
2020-11-13 16:22:21 +0800

    release ysssh 0.0.4 by bot 2020-11-13-16-22

--- ysssh.rb
+++ ysssh.rb
@@ -1,16 +1,16 @@
 class Ysssh < Formula
     desc "Devops tools 运维工具 ysssh"
     homepage "https://github.com/ysicing/ssh"
-    version "0.0.3"
+    version "0.0.4"
     bottle :unneeded
 
     if OS.mac?
       url "https://github.com/ysicing/ssh/releases/download/#{version}/ssh_darwin_amd64"
-      sha256 "a665d42978f252f7ac804cc51b7baf0486f0309a9a712dc1b531ddcde3520bbf"
+      sha256 "bad26866c290e1d27b7e8876e22a0a1d81bb97a188417e05c74108bde865c691"
     elsif OS.linux?
       if Hardware::CPU.intel?
         url "https://github.com/ysicing/ssh/releases/download/#{version}/ssh_linux_amd64"
-        sha256 "a665d42978f252f7ac804cc51b7baf0486f0309a9a712dc1b531ddcde3520bbf"
+        sha256 "bad26866c290e1d27b7e8876e22a0a1d81bb97a188417e05c74108bde865c691"
       end
     end
 
