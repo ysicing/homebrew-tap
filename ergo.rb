a9796242a9b290b1d24b04102885f0fdca39d85d
hkbot <hkbot@hk2.vps>
2020-12-23 20:04:09 +0800

    release ergo 1.0.12 by bot 2020-12-23-20-04

--- ergo.rb
+++ ergo.rb
@@ -1,16 +1,16 @@
 class Ergo < Formula
     desc "Devops tools 运维工具"
     homepage "https://github.com/ysicing/ergo"
-    version "1.0.11"
+    version "1.0.12"
     bottle :unneeded
 
     if OS.mac?
       url "https://github.com/ysicing/ergo/releases/download/#{version}/ergo_darwin_amd64"
-      sha256 "33e769e05bd2dc1f604a9dca71b9c8e70745d72d5b38ac7fb87c6b9f2bccba44"
+      sha256 "a3dc2e32eeeba2d0e1c0914ccc4ee415134dc3512d361e0ee7fd285232825614"
     elsif OS.linux?
       if Hardware::CPU.intel?
         url "https://github.com/ysicing/ergo/releases/download/#{version}/ergo_linux_amd64"
-        sha256 "7316b4231f324a39af88c8de22ea4f7b186ac8137d22c114a021ac1b0ac8af3f"
+        sha256 "ed4c9c5c603f6ba7ac3c7409100c36c9e5f7d863692af302f2d7264e952108e8"
       end
     end
 
