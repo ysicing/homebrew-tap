256c57304ff98bb270b07c8dd5be45a982cea67a
ysbot <ysbot@hk-in.ysicing.local>
2021-09-29 18:09:15 +0800

    release ergo 1.0.0 by bot 2021-09-29-18-09

--- air.rb
+++ air.rb
@@ -2,20 +2,29 @@
 class Air < Formula
     desc "Live reload for Go apps"
     homepage "https://github.com/cosmtrek/air"
-    version "1.12.4"
+    version "1.27.3"
     bottle :unneeded
 
     if OS.mac?
-      url "https://github.com/cosmtrek/air/releases/download/v#{version}/air_#{version}_darwin_amd64"
-      sha256 "f0b8fd407c598bc729c80a4393dec2b13b334fe3676f7e9bd2adf6ed132854ab"
+      if Hardware::CPU.arm?
+        url "https://github.com/cosmtrek/air/releases/download/v#{version}/air_#{version}_darwin_arm64"
+        sha256 "513459b8f737049ffa66cbdc91d785d676b164d3a79deb064562382f33eee4db"
+      else
+        url "https://github.com/cosmtrek/air/releases/download/v#{version}/air_#{version}_darwin_amd64"
+        sha256 "41799175111823a992cb65325e2cdd5badc97cdcc0b3abd340d47bb30cb33bc7"
+      end
     elsif OS.linux?
       if Hardware::CPU.intel?
         url "https://github.com/cosmtrek/air/releases/download/v#{version}/air_#{version}_linux_amd64"
-        sha256 "104538accc548cc8d3779aed07794e77d1674088baad5d4e73e91f9576eb6244"
+        sha256 "68f8c0b1fb81fc2cda5fd2b16b857416f8c570072d0d7eaa06588b9bce3e2366"
       end
     end
 
     def install
-      bin.install "air_${version}_darwin_amd64" => "air"
+      if Hardware::CPU.intel?
+        bin.install "air_${version}_darwin_amd64" => "air"
+      else
+        bin.install "air_${version}_darwin_arm64" => "air"
+      end
     end
   end
--- ergo.rb
+++ ergo.rb
@@ -1,29 +1,20 @@
 class Ergo < Formula
     desc "Devops tools 运维工具"
     homepage "https://github.com/ysicing/ergo"
-    version "1.0.21"
+    version "1.0.0"
     bottle :unneeded
 
     if OS.mac?
-      if Hardware::CPU.arm?
-        url "https://github.com/ysicing/ergo/releases/download/#{version}/ergo_darwin_arm64"
-        sha256 "7784ca2580dd858e72b12332096b07f06065592335ac711b074eeab036ce7d0f"
-      else
-        url "https://github.com/ysicing/ergo/releases/download/#{version}/ergo_darwin_amd64"
-        sha256 "bccdd4b818bb9b750bf41b2f15c222c70708f075183cdee2bc22d10ac6eabd4d"
-      end  
+      url "https://github.com/ysicing/ergo/releases/download/#{version}/ergo_darwin_amd64"
+      sha256 "682ef43601a5071c0cc606924263d20e8f657f52d072bdcc88333536dcf737eb"
     elsif OS.linux?
       if Hardware::CPU.intel?
         url "https://github.com/ysicing/ergo/releases/download/#{version}/ergo_linux_amd64"
-        sha256 "06ecbbbedd0530cd61d4a33a6d11908b843e3877b23ca5307849138f3f421451"
+        sha256 "d7bf63c2b4680750338255a16becac06b14b2328a72e1dcdec21f27045d60f54"
       end
     end
 
     def install
-      if Hardware::CPU.intel?
-        bin.install "ergo_darwin_amd64" => "ergo"
-      else
-        bin.install "ergo_darwin_arm64" => "ergo"
-      end 
+      bin.install "ergo_darwin_amd64" => "ergo"
     end
   end
