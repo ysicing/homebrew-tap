8de7bef3e4b4932efe74b5fa2bd8a7ca0dbdb536
ysbot <ysbot@hk-in.ysicing.local>
2021-09-30 14:05:47 +0800

    release ergo 2.0.0-beta by bot 2021-09-30-14-05

--- ergo.rb
+++ ergo.rb
@@ -1,20 +1,29 @@
 class Ergo < Formula
-    desc "Devops tools 运维工具"
+    desc "Devops tools 运维工具Ergo"
     homepage "https://github.com/ysicing/ergo"
-    version "1.0.0"
+    version "2.0.0-beta"
     bottle :unneeded
 
     if OS.mac?
-      url "https://github.com/ysicing/ergo/releases/download/#{version}/ergo_darwin_amd64"
-      sha256 "682ef43601a5071c0cc606924263d20e8f657f52d072bdcc88333536dcf737eb"
+      if Hardware::CPU.arm?
+        url "https://github.com/ysicing/ergo/releases/download/#{version}/ergo_darwin_arm64"
+        sha256 "06be598d284a160cf2bd0050b65264b40cf38e32609bc51a691d29508dca1a08"
+      else
+        url "https://github.com/ysicing/ergo/releases/download/#{version}/ergo_darwin_amd64"
+        sha256 "84ca6072992d93cf8ad83af19784914f27b56d283941a6e2ed77b5d53a32a337"
+      end  
     elsif OS.linux?
       if Hardware::CPU.intel?
         url "https://github.com/ysicing/ergo/releases/download/#{version}/ergo_linux_amd64"
-        sha256 "d7bf63c2b4680750338255a16becac06b14b2328a72e1dcdec21f27045d60f54"
+        sha256 "6967c0a94fcf4f3bb75ceaacce0ad9d3b23cb03085021ecb63a183aef95dbf9c"
       end
     end
 
     def install
-      bin.install "ergo_darwin_amd64" => "ergo"
+      if Hardware::CPU.intel?
+        bin.install "ergo_darwin_amd64" => "ergo"
+      else
+        bin.install "ergo_darwin_arm64" => "ergo"
+      end 
     end
   end
