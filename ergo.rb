367bf3c195d7c9d0e1e31545d62da98f7fbad6ff
ysbot <ysbot@hk-in.ysicing.local>
2021-10-12 19:27:44 +0800

    release ergo 2.0.2 by bot 2021-10-12-19-27

--- ergo.rb
+++ ergo.rb
@@ -1,21 +1,21 @@
 class Ergo < Formula
     desc "Devops tools 运维工具Ergo"
     homepage "https://github.com/ysicing/ergo"
-    version "2.0.1"
+    version "2.0.2"
     bottle :unneeded
 
     if OS.mac?
       if Hardware::CPU.arm?
         url "https://github.com/ysicing/ergo/releases/download/#{version}/ergo_darwin_arm64"
-        sha256 "3b2a01807e8c732a35547e46ec4e66b748bcdbcf3a164e95d415f6b7c8797920"
+        sha256 "214a44ff6d348f0bcc3f18721725b98c77671ca4dc0e53394110db89de2a7386"
       else
         url "https://github.com/ysicing/ergo/releases/download/#{version}/ergo_darwin_amd64"
-        sha256 "65d522398688b4e6a87054fcba4cffb608093be80339a7b056d9996cbc45bdad"
+        sha256 "a1f2f40f8d06a230b6cf8290f2e1b12a659ca7c3f987c9de1af2c2f7682d248f"
       end  
     elsif OS.linux?
       if Hardware::CPU.intel?
         url "https://github.com/ysicing/ergo/releases/download/#{version}/ergo_linux_amd64"
-        sha256 "cfb1aa9484ec615e7451534915da1c8efb5f47d5db9eacbaf0fd633c21eee7a0"
+        sha256 "0ccb2dd26b7514bc70563c9d76640f7f4a16fbba4a35c46659e2a1709337dadb"
       end
     end
 
