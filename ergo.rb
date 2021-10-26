49ae5b95474f78e07cfa5f61268082a01c27a02b
root <root@hk1.vps.godu.dev>
2021-10-26 18:59:02 +0800

    release ergo 2.3.0 by bot 2021-10-26-18-59

--- ergo.rb
+++ ergo.rb
@@ -7,15 +7,15 @@ class Ergo < Formula
     if OS.mac?
       if Hardware::CPU.arm?
         url "https://github.com/ysicing/ergo/releases/download/#{version}/ergo_darwin_arm64"
-        sha256 "758357fad8c03cfb3cf62746c8410624af98a7e70ff64bec0f4dd662f4fe02b9"
+        sha256 "2edbb6c5b22649399ffbba212aa6485f9c25d496fecd5682775e371b98463a12"
       else
         url "https://github.com/ysicing/ergo/releases/download/#{version}/ergo_darwin_amd64"
-        sha256 "265b3633b08ae4531c977429ac9d1a9df6722e1e7a65a7531701788d7849c1a6"
+        sha256 "72093ac2c904424437d7c7e5c12befece7cc9dd75074e7798bcaa75274b03208"
       end  
     elsif OS.linux?
       if Hardware::CPU.intel?
         url "https://github.com/ysicing/ergo/releases/download/#{version}/ergo_linux_amd64"
-        sha256 "713ec9ee8f0d11b7af5a98ca5d4104184006071978deb6ee2ca5239c210667a3"
+        sha256 "70c83cdb7dd996629d09a83aad21134be4328af653c5b7930834a725d192c15c"
       end
     end
 
--- genergo.sh
+++ genergo.sh
@@ -4,7 +4,7 @@ set -e
 
 cd /root/homebrew-tap
 docker pull ysicing/taprb:ergo
-docker run -it -v $PWD:/data --rm ysicing/taprb:ergo cp -a /ergo.rb /data/ergo.rb
+docker run -i -v $PWD:/data --rm ysicing/taprb:ergo cp -a /ergo.rb /data/ergo.rb
 git add .
 version=$(cat ergo.rb | grep "version " | awk -F\" '{print $2}')
 datetime=$(date +%F-%H-%M)
