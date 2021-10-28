4ee7d37a52d9c62f7777c1e719af8e0ab9b50195
root <root@hk1.vps.godu.dev>
2021-10-28 23:32:59 +0800

    release ergo 2.4.0 by bot 2021-10-28-23-32

--- ergo.rb
+++ ergo.rb
@@ -1,20 +1,20 @@
 class Ergo < Formula
     desc "Devops tools 运维工具Ergo"
     homepage "https://github.com/ysicing/ergo"
-    version "2.4.0-beta"
+    version "2.4.0"
 
     if OS.mac?
       if Hardware::CPU.arm?
         url "https://github.com/ysicing/ergo/releases/download/#{version}/ergo_darwin_arm64"
-        sha256 "9928e9fd049fb7443f61b567f5d2e12ac736c4b0137d83638a4fbb020e9d87ea"
+        sha256 "4fec72a0e27c8a1f8b5b0277ecb5d656385cd29086ee7af2ec7fb4347056539d"
       else
         url "https://github.com/ysicing/ergo/releases/download/#{version}/ergo_darwin_amd64"
-        sha256 "2f75ed8e0664d505f4f072e0a47b0d8576484418d52ddc399bba0545c47dc40c"
+        sha256 "c9cf6f05f75a5c7a3689e6c91d12516cd36e8834820884da71c5d7fb55bd787b"
       end  
     elsif OS.linux?
       if Hardware::CPU.intel?
         url "https://github.com/ysicing/ergo/releases/download/#{version}/ergo_linux_amd64"
-        sha256 "f75d94b1f3a17bcbc621aced083097de0dad39bf1c700d3a1183df96c4ad42f3"
+        sha256 "24392da45c57cffb07b95cc4e6d0706ebc7f4eca199e3b679fe2c1312afe107b"
       end
     end
 
