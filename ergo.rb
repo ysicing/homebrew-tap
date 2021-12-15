712c99d1ee42f2e2c21e9d31c8d16acde7cb7767
root <root@hk1.vps.godu.dev>
2021-12-15 18:02:31 +0800

    release ergo 2.6.12 by bot 2021-12-15-18-02

--- ergo.rb
+++ ergo.rb
@@ -1,24 +1,24 @@
 class Ergo < Formula
     desc "Devops tools 运维工具Ergo"
     homepage "https://github.com/ysicing/ergo"
-    version "2.6.11"
+    version "2.6.12"
 
     if OS.mac?
       if Hardware::CPU.arm?
         url "https://github.com/ysicing/ergo/releases/download/#{version}/ergo_darwin_arm64"
-        sha256 "4fed5db12c42c01d39615279f82c6b35236243a721017c6872bf32f6bd7db134"
+        sha256 "178583701dbd5f1d3d277e98da063159788a0942d446281cb93571cce890df07"
       else
         url "https://github.com/ysicing/ergo/releases/download/#{version}/ergo_darwin_amd64"
-        sha256 "ca28896b8dc376838d837fa52e5f83b11e542d212e133ff4dd470dae4ec3fd8c"
+        sha256 "1cb2bb09eade4a4ddce7d24f2286b48e51e9d2aaaf02bb3f0a978d3ecc6047d3"
       end  
     elsif OS.linux?
       if Hardware::CPU.intel?
         url "https://github.com/ysicing/ergo/releases/download/#{version}/ergo_linux_amd64"
-        sha256 "8877f09fe3d744756d5c98e1ace184b7287c86bb1a0855da6c84fb615f33c681"
+        sha256 "e0a3343bb1a8c070becbeab893d3547a2c1f5024ee474b09c171c2313ce20712"
       end
       if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
         url "https://github.com/ysicing/ergo/releases/download/#{version}/ergo_linux_arm64"
-        sha256 "6f5b8158cd3c3264e17febd0a82c4fd99136f789835bf97fbcfe11a810adbd34"
+        sha256 "d5990f5a70020da92079eaa1e853dbad7bdb557bf835f96d2f8d967707366d36"
       end
     end
 
