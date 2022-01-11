83f6386f5f537bf45078c297c2f72cbf611c25de
缘生 <i@ysicing.me>
2022-01-11 16:21:11 +0800

    Update air.rb

--- air.rb
+++ air.rb
@@ -2,20 +2,20 @@
 class Air < Formula
     desc "Live reload for Go apps"
     homepage "https://github.com/cosmtrek/air"
-    version "1.27.5"
+    version "1.27.8"
 
     if OS.mac?
       if Hardware::CPU.arm?
         url "https://github.com/cosmtrek/air/releases/download/v#{version}/air_#{version}_darwin_arm64"
-        sha256 "a70d22b01b8b84f98afb7401f4b191172fbdf1749f86b5e26c71e7a9fb4f3c6a"
+        sha256 "f0238d518c68e1512af81e92e0acb8b9ff7d105ccffade4e4bbc86013f746e6d"
       else
         url "https://github.com/cosmtrek/air/releases/download/v#{version}/air_#{version}_darwin_amd64"
-        sha256 "28af8f514f2513b32975ca17ba3aea3472ea22361e9db7eb804bd9b21e4d757b"
+        sha256 "d902bd2854ac1c63d49f5ce7cba904c0139528b922a1d03be1191b064cbf64c9"
       end
     elsif OS.linux?
       if Hardware::CPU.intel?
         url "https://github.com/cosmtrek/air/releases/download/v#{version}/air_#{version}_linux_amd64"
-        sha256 "d6f058729c2b6b959d086055c81f9c5838ac6c0e3be9a04d3c5d3986e2f035a6"
+        sha256 "cb9a157608c8aaf4438ede02eaed883c65e661af5aff446c90a6d081b370a232"
       end
     end
 
