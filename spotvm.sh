ba3f0da3f36d0326e3fa60d74ccefe8aeab14e53
ysbot <ysbot@hk-in.ysicing.local>
2022-09-13 10:14:15 +0800

    release spot 0.0.2 by bot 2022-09-13-10-14

--- spotvm.sh
+++ spotvm.sh
@@ -5,7 +5,7 @@ set -e
 cd /root/homebrew-tap
 
 git add .
-version=$(cat spot.rb | grep "version " | awk -F\" '{print $2}')
+version=$(cat spotvm.rb | grep "version " | awk -F\" '{print $2}')
 datetime=$(date +%F-%H-%M)
 git commit -m "release spot ${version} by bot ${datetime}"
 git push origin master
