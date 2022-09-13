8fefed58743991d09307cd4b3b70f1b8df3318cd
ysbot <ysbot@hk-in.ysicing.local>
2022-09-13 10:13:48 +0800

    release spot  by bot 2022-09-13-10-13

--- spotvm.rb
+++ spotvm.rb
@@ -1,24 +1,24 @@
 class Spotvm < Formula
     desc "spot vm tool"
     homepage "https://github.com/ysicing/spot"
-    version "0.0.1"
+    version "0.0.2"
 
     if OS.mac?
       if Hardware::CPU.arm?
         url "https://github.com/ysicing/spot/releases/download/v#{version}/spot_darwin_arm64"
-        sha256 "d84cd9f12b770b09e4a766db48349e5874ddeeef330c2d38c8b494ebef227cc7"
+        sha256 "223adcf922797aa1c4e3ead9c989b1738f6276e7d5de6028a92ab29144519275"
       else
         url "https://github.com/ysicing/spot/releases/download/v#{version}/spot_darwin_amd64"
-        sha256 "cd5106de14c0b3caecd475aed6603ea9b2a16a81c99eec7802a87c3a1c2407f3"
+        sha256 "4eb4b601a889e0d49111e89a3e336d201013618a92cbf58cde694c4c42f441ae"
       end
     elsif OS.linux?
       if Hardware::CPU.intel?
         url "https://github.com/ysicing/spot/releases/download/v#{version}/spot_linux_amd64"
-        sha256 "11038a71d30c4d0ade6ef7646c7ac47c13dc8b4cfbcb2c6f6db161a9eb38df87"
+        sha256 "0f5ed138d251008636fede677efbb5fa5b85b8591b38a4b2ba8624c0cd9f108b"
       end
       if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
         url "https://github.com/ysicing/spot/releases/download/v#{version}/spot_linux_arm64"
-        sha256 "46092bcdf2b8157af568f6751ffa0ca9a3b9a4335ea3b925dcd2ccbc131216d9"
+        sha256 "7a4d984e6df4d3b6c080d222f7f816c3836977198eb8c0ed20ef986e41cdce3d"
       end
     end
 
