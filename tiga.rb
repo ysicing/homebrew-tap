981b1c0253f4ed2d98744d539049fc54124e252c
ysbot <ysbot@hk-in.ysicing.local>
2023-05-06 10:26:21 +0800

    release tiga 0.0.5 by bot 2023-05-06-10-26

--- tiga.rb
+++ tiga.rb
@@ -5,20 +5,20 @@ class Tiga < Formula
 
     if OS.mac?
       if Hardware::CPU.arm?
-        url "https://github.com/ysicing/tiga/releases/download/#{version}/tiga_darwin_arm64"
-        sha256 "f1795d17ee7a273c84a12a9ca1be16d010f3fe6c4474f6dc483ab3fe45d54ec5"
+        url "https://ghproxy.com/https://github.com/ysicing/tiga/releases/download/#{version}/tiga_darwin_arm64"
+        sha256 "6cb5eccb075b23b72b74143f301725ea81b6a983fa158ce26fb4a47008d8ffa7"
       else
-        url "https://github.com/ysicing/tiga/releases/download/#{version}/tiga_darwin_amd64"
-        sha256 "d046e21f8932727337481efa55cd726a8d7c8c8120959e422580d667667aede2"
+        url "https://ghproxy.com/https://github.com/ysicing/tiga/releases/download/#{version}/tiga_darwin_amd64"
+        sha256 "a72c72c1147b427a65134ddaa41334eb900044c49f03585398c3bfb30fd48266"
       end
     elsif OS.linux?
       if Hardware::CPU.intel?
-        url "https://github.com/ysicing/tiga/releases/download/#{version}/tiga_linux_amd64"
-        sha256 "b1ec94c42123b5fd34a9413f000dbe88e017f613e90e94eb83fc2ac12af1bb22"
+        url "https://ghproxy.com/https://github.com/ysicing/tiga/releases/download/#{version}/tiga_linux_amd64"
+        sha256 "27fbebb8d058b98a9dec7dbec2e55f538370fa4cb563850561ce759319a0c01e"
       end
       if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-        url "https://github.com/ysicing/tiga/releases/download/#{version}/tiga_linux_arm64"
-        sha256 "98a955517ef298727fee6120e976c2c0cbd4d3ea70b3e6ad8d19834e8d2fa260"
+        url "https://ghproxy.com/https://github.com/ysicing/tiga/releases/download/#{version}/tiga_linux_arm64"
+        sha256 "f99ef5dd15dfa8b3923c216e4353885ed67452a74088daaa8ee2da706bc8b440"
       end
     end
 
