73ff916d5aee2aa70b020fae680a45bd7e0345e6
ysbot <ysbot@hk-in.ysicing.local>
2023-05-08 23:22:44 +0800

    release tiga 0.0.8 by bot 2023-05-08-23-22

--- tiga.rb
+++ tiga.rb
@@ -1,44 +1,45 @@
 class Tiga < Formula
-    desc "Simple and powerful tool for sernior restart engineer"
+    desc "Simple and powerful tool for senior restart engineer"
     homepage "https://github.com/ysicing/tiga"
-    version "0.0.7"
+    version "0.0.8"
 
-    if OS.mac?
+    on_macos do
       if Hardware::CPU.arm?
         url "https://github.com/ysicing/tiga/releases/download/v#{version}/tiga_darwin_arm64"
-        sha256 "745cbc099a52279ff65db7f3a9a9056eab19d4ccf5c51852bf14c1c3a48daec0"
-      else
-        url "https://github.com/ysicing/tiga/releases/download/v#{version}/tiga_darwin_amd64"
-        sha256 "7ca995f7bfb440661057b08c8aead6f543e385dcab2f7090af6763837eea8854"
+        sha256 "ebfcece17e5209c8025a791f30c0dd4ec629bf20bcae89a03444dc482f54a381"
+
+        def install
+            bin.install "tiga"
+        end
       end
-    elsif OS.linux?
+
+      if Hardware::CPU.intel?
+        url "https://github.com/ysicing/tiga/releases/download/v#{version}/tiga_darwin_amd64"
+        sha256 "aa4d58c5982fbb6887f5a3b8f4a7f4595128b205829b2b6c1d06ddbaf6cfceb6"
+
+        def install
+            bin.install "tiga"
+        end
+      end
+    end
+
+    on_linux do
       if Hardware::CPU.intel?
         url "https://github.com/ysicing/tiga/releases/download/v#{version}/tiga_linux_amd64"
-        sha256 "935198d9ec266940f9333db5e78b2754f8386ca33f3e057965f18606e28bf4cc"
+        sha256 "3e893a71aa4f1b161b2aba800dcb4ff24da2341d38f04984f7660b8854728903"
+
+        def install
+            bin.install "tiga"
+        end
       end
+
       if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
         url "https://github.com/ysicing/tiga/releases/download/v#{version}/tiga_linux_arm64"
-        sha256 "f604dab942e77d2bff98bc46fca37fd9e6334ddd23a720485f7f14ad4368ae29"
-      end
-    end
+        sha256 "2c285fb534202b7659045cfb22f78a92e46d4d605e4e35728c9b228a77ff4a5d"
 
-    def install
-      if OS.mac?
-        if Hardware::CPU.intel?
-          bin.install "tiga_darwin_amd64" => "tiga"
-        else
-          bin.install "tiga_darwin_arm64" => "tiga"
-        end
-      elsif OS.linux?
-        if Hardware::CPU.intel?
-          bin.install "tiga_linux_amd64" => "tiga"
-        else
-          bin.install "tiga_linux_arm64" => "tiga"
+        def install
+            bin.install "tiga"
         end
       end
-    end
-
-    test do
-      assert_match "tiga vervion v#{version}", shell_output("#{bin}/tiga version")
     end
 end
