a3ecf8fc5c0148c99a9e7094cee7522535c1a399
ysicing <i@ysicing.me>
2026-09-22 12:22:30 +0800

    xstats 0.7.0

---
+++ Casks/xstats.rb
@@ -0,0 +1,21 @@
+cask "xstats" do
+  version "0.7.0"
+  sha256 "c5d422d7647aa36da235595131b9d3186f85afbeb93233f8ee14de2d2526ae06"
+
+  url "https://cos.ysicing.net/oss/apps/macOS/XStats/XStats-#{version}-AppleSilicon.dmg"
+  name "XStats"
+  desc "Menu bar system monitor with fan control, keep-awake and cleanup"
+  homepage "https://github.com/ysicing/xstats"
+
+  # 应用内置在线升级，brew upgrade 默认不再重复升级
+  auto_updates true
+  depends_on arch: :arm64
+  depends_on macos: :sonoma
+
+  app "XStats.app"
+
+  zap trash: [
+    "~/Library/Logs/XStats",
+    "~/Library/Preferences/work.12306.xstats.app.plist",
+  ]
+end
