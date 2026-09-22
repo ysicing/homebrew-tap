cefba6d0203e0d0477f65dec7e7e4b71218101f0
ysicing <i@ysicing.me>
2026-09-22 12:45:23 +0800

    xstats 0.7.0: use c-ip origin

--- Casks/xstats.rb
+++ Casks/xstats.rb
@@ -2,7 +2,7 @@ cask "xstats" do
   version "0.7.0"
   sha256 "c5d422d7647aa36da235595131b9d3186f85afbeb93233f8ee14de2d2526ae06"
 
-  url "https://cos.ysicing.net/oss/apps/macOS/XStats/XStats-#{version}-AppleSilicon.dmg"
+  url "https://c.ysicing.net/oss/apps/macOS/XStats/XStats-#{version}-AppleSilicon.dmg"
   name "XStats"
   desc "Menu bar system monitor with fan control, keep-awake and cleanup"
   homepage "https://github.com/ysicing/xstats"
